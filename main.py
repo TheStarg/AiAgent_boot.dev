c0d4fd4391b473484ce4c7fcc03e3bb2eab826db
TheStarg <johndgidcumb@gmail.com>
2025-12-22 06:34:30 -0500

    setting up continued

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key == None:
+    raise RuntimeError("api_key == None")
+
+client = genai.Client(api_key = api_key)
+
+
 def main():
     print("Hello from AiAgent!")
 
