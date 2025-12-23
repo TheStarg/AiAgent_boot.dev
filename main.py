e530d02c8fb823f116b93faa0e8bfa9ee9c809b4
TheStarg <johndgidcumb@gmail.com>
2025-12-23 02:49:08 -0500

    Takes a commandline prompt
    Uses argparse to take a positional argument as a prompt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
+import os, argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 def main():
     print("Hello from AiAgent!")
     load_dotenv()
@@ -9,9 +11,14 @@ def main():
     if api_key == None:
         raise RuntimeError("api_key == None")
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User Prompt")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     client = genai.Client(api_key = api_key)
     response = client.models.generate_content(
-        model = "gemini-2.5-flash", contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model = "gemini-2.5-flash", contents = messages
     )
     if response.usage_metadata == None:
         raise Exception("Exception: response has usage_metadata = None")
@@ -19,6 +26,7 @@ def main():
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response: {response.text}")
 
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 
 
