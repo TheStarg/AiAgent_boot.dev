fe2c1b8a7efcfcb11cd060c5a4075f389da38c65
TheStarg <johndgidcumb@gmail.com>
2025-12-23 02:11:11 -0500

    Returns a hardcoded prompt and prompt and response tokens

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ def main():
     response = client.models.generate_content(
         model = "gemini-2.5-flash", contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     )
-    print(response.text)
+    if response.usage_metadata == None:
+        raise Exception("Exception: response has usage_metadata = None")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response: {response.text}")
 
 
 
